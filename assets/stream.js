792e85860b0f56ebc583a223eadb424dd012d0e9
Andrews <andrewschiozo@gmail.com>
2018-11-13 15:42:49 -0200

    Create stream.js

---
+++ assets/stream.js
@@ -0,0 +1,38 @@
+$(function(){
+	loadMusicas();
+	 //Search
+	$("#pesquisar").on("keyup", function() {
+		var value = $(this).val().toLowerCase();
+		$(".list-group li").filter(function() {
+		  $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+
+	// On menu item click
+	$('#menu').on('click', '.nav-item', function(data){
+		$('#btnMenu').trigger('click');
+	});
+
+	//On list item click
+	$('#content').on('click', '.list-group > li', function(data){
+		$('source').attr('src', $(this).attr('url'));
+		var audio = document.getElementsByTagName('audio')[0];
+		audio.load();
+	});
+});
+
+function loadMusicas()
+{
+	$.get('musicList.php', function(response){
+		response = JSON.parse(response);
+	    $.each(response.data, function(index, value){
+	    	var item = $('<li></li>');
+	    	item.addClass('list-group-item');
+	    	item.addClass('list-group-item-action');
+	    	item.attr('url', value.file);
+	    	item.text(value.artist + ' - ' + value.title);
+	    	$('#musicas').append(item);
+   		});
+	});
+}
+
